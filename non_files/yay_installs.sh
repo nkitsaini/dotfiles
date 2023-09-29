f2c14304300e05d5fd082ab1b21d6ff16466f1d4
Ankit Saini <nnkitsaini@gmail.com>
2023-09-29 15:46:58 +0530

    partial yay-install stuff

---
+++ non_files/yay_installs.sh
@@ -0,0 +1,169 @@
+PACKAGES=(
+    # Battery
+    batsignal
+    tlp
+alacritty
+android-file-transfer
+aria2
+base-devel
+bat
+batsignal
+bc
+bluez-utils
+brightnessctl
+bumblebee-status
+caddy
+chezmoi
+chromium
+dbeaver
+docker-compose
+dolphin
+dpkg
+dunst
+dust
+exa
+fd
+feh
+firefox
+git-delta
+github-cli
+gnome-terminal
+gnu-netcat
+gnumeric
+go
+gparted
+helixbinhx
+hexyl
+hplip
+htop
+httpie
+hwatch
+i3-wm
+i3blocks
+jq
+kopia-bin
+kubectl
+
+libcs50 11.0.2-1
+libiptcdata 1.0.5-2
+libosinfo 1.10.0-3
+libqt6xdg-git 3.11.0.1.g4bb4624-1
+libreoffice-fresh 7.6.0-3
+logseq-desktop-bin 0.9.15-1
+lshw B.02.19.2-6
+lssecret-git r10.20fd771-2
+lunarvim-git 1.2.0.r191.ge548d6bc-1
+man-db 2.11.2-1
+man-pages 6.05.01-1
+mariadb 11.1.2-1
+mold 2.1.0-1
+mongodb-compass 1.39.3-1
+mosh 1.4.0-4
+mpv 1:0.36.0-1
+mysql-workbench 8.0.34-1
+nano 7.2-1
+ncdu 2.3-1
+nethogs 0.8.7-1
+ngrok 3.3.4-1
+novnc 1.4.0-1
+nyxt 3.6.0-1
+obs-studio 29.1.3-1
+obsidian 1.3.7-1
+okular 23.08.0-2
+oxygen 5.27.7-1
+p7zip 1:17.05-1
+parallel 20230822-1
+pavucontrol 1:5.0+r64+geba9ca6-1
+perf 6.3-5
+plasma-browser-integration 5.27.7-1
+plasma-desktop 5.27.7.1-1
+plasma-disks 5.27.7-1
+plasma-firewall 5.27.7-1
+plasma-pa 5.27.7-1
+plasma-sdk 5.27.7.1-1
+plasma-systemmonitor 5.27.7-1
+plasma-thunderbolt 5.27.7-1
+plasma-vault 5.27.7-1
+plasma-welcome 5.27.7-1
+postgresql 15.4-2
+powertop 2.15-1
+progress 0.16-1
+pyenv 1:2.3.25-1
+pypy3 7.3.12-1
+python-iwlib 1.7.0-5
+python-virtualenv 20.24.3-1
+qbittorrent 4.5.5-1
+qt6-tools 6.5.2-2
+rclone 1.63.1-1
+restic 0.16.0-1
+rsync 3.2.7-4
+scrot 1.10-1
+sd 0.7.6-2
+sddm-kcm 5.27.7-1
+simplescreenrecorder 0.4.4-2
+sioyek 2.0.0-3
+skanlite 23.08.0-1
+snapd 2.60.2-1
+sox 14.4.2+r182+g42b3557e-3
+starship 1.16.0-1
+sublime-text-4 4.4152-2
+sway 1:1.8.1-1
+tableplus 0.1.220-1
+tcpdump 4.99.4-1
+terminator 2.1.3-3
+texlive-bibtexextra 2023.66594-19
+texlive-binextra 2023.66594-19
+texlive-context 2023.66594-19
+texlive-fontsextra 2023.66594-19
+texlive-fontutils 2023.66594-19
+texlive-formatsextra 2023.66594-19
+texlive-games 2023.66594-19
+texlive-humanities 2023.66594-19
+texlive-latexextra 2023.66594-19
+texlive-luatex 2023.66594-19
+texlive-mathscience 2023.66594-19
+texlive-metapost 2023.66594-19
+texlive-music 2023.66594-19
+texlive-pstricks 2023.66594-19
+texlive-publishers 2023.66594-19
+texlive-xetex 2023.66594-19
+thunar 4.18.7-1
+tig 2.5.8-1
+tigervnc 1.13.1-2
+tldr 3.2.0-1
+tlp 1.6.1-1
+tmate 2.4.0-3
+tmuxp 1.29.0-1
+tokei 12.1.2-1
+torrent-file-editor 0.3.18-2
+tree 2.1.1-1
+ttf-fira-code 6.2-2
+ttf-fira-sans 1:4.301-2
+ttyd 1.7.3-1
+typst 1:0.7.0-1
+typst-lsp 0.9.5-1
+valgrind 3.21.0-4
+vault 1.14.1-1
+visual-studio-code-bin 1.81.1-1
+vlc 3.0.18-15
+vscode-langservers-extracted 4.7.0-1
+weston 12.0.2-1
+wireguard-tools 1.0.20210914-1
+xbindkeys 1.8.7-4
+xdotool 3.20211022.1-1
+xf86-video-amdgpu 23.0.0-1
+xf86-video-ati 1:22.0.0-1
+xf86-video-nouveau 1.0.17-2
+xf86-video-vmware 13.4.0-1
+xh 0.18.0-1
+yadm 3.2.2-1
+yay 12.1.2-1
+zip 3.0-10
+zram-generator 1.1.2-1
+
+)
+
+echo "${PACKAGES[@]}"
+# exec yay -Syy \
+#    batsingal \
+#    tlp \
