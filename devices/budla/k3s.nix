fad7d089b5f875f94b33da161f3803062f13dedb
Ankit Saini <nnkitsaini@gmail.com>
2024-03-16 20:07:09 +0530

    add custom registry to k3s

--- devices/budla/k3s.nix
+++ devices/budla/k3s.nix
@@ -2,4 +2,12 @@
   networking.firewall.allowedTCPPorts = [ 6443 80 443 ];
   networking.firewall.allowedUDPPorts = [ 6443 ];
   services.k3s.enable = true;
+
+environment.etc."rancher/k3s/registries.yaml".text = ''
+mirrors:
+  budla.lan:5000:
+    endpoint:
+      - "http://budla.lan:5000"
+'';
+  
 }
