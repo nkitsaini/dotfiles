5687dbac4dcdc95272086bf2de6e20964441b969
Ankit Saini <nnkitsaini@gmail.com>
2024-03-18 17:43:50 +0530

    notes for next time

--- devices/budla/disco.nix
+++ devices/budla/disco.nix
@@ -1,3 +1,12 @@
+# TODO: Next time, turn this into real nixos module, as mentioned in: https://github.com/nix-community/disko/blob/master/docs/quickstart.md (just modify for flakes)
+# That will bring down the installations to a fix number of steps along with building a tailored ISO.
+# - Get keys from password manager (keep bitwarden in image)
+# - Setup ssh authorized_keys from github (keep a binary to do this, actually make it do everything, or print all steps)
+# - clone the git repo (have git, tmux stuff in image)
+# - run this file to create partitions (have disko pre-installed in image)
+# - run `nixos-generate-config --show-hardware-config --no-filesystems` and save to this directory  (also remove the hardware-configuration from here, so that it is an error, or do something like {todo.add-hardware-config = ''This will fail. Run command to generate config''}})
+# - run `nixos-install --flake ...`
+
 {
   disko.devices = {
     disk = {
@@ -27,18 +36,6 @@
                 resumeDevice = true; # resume from hiberation from this device
               };
             };
-            # swap = {
-            #   label = "boot";
-            #   name = "ESP";
-            #   priority = 1;
-            #   size="512M";
-            #   type = "EF00";
-            #   content = {
-            #     type = "filesystem";
-            #     format = "vfat";
-            #     mountpoint = "/boot";
-            #   };
-            # };
             root = {
               label = "nixos";
               size = "100%";
@@ -64,9 +61,6 @@
                     mountpoint = "/nix";
                   };
                 };
-
-                # What the heck is this?
-                mountpoint = "/partition-root";
               };
             };
           };
