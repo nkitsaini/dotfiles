02817ceda6d5cdcf8784a1e8090650a45195ba94
Ankit Saini <asaini@singlestore.com>
2024-04-08 12:09:12 +0530

    document yt-dlp  flags for audio playlist

--- packages/hm/yt-dlp.nix
+++ packages/hm/yt-dlp.nix
@@ -1,4 +1,7 @@
 {
+  # yt-dlp "<playlist-url>" --download-archive archive.txt -f 'ba/best'                                          -x                  --audio-format mp3  --audio-quality 0                --embed-metadata            --embed-thumbnail
+  #                         ^-- incremental download |      ^ first try best-audio only, then try best overall   ^ extract audio     ^ convert to mp3    ^ with best quality (15 worst)   ^ keep album/artist stuff   ^ Keep thumbnail       
+  # 
   programs.yt-dlp.enable = true;
   programs.yt-dlp.settings = {
     embed-thumbnail = true;
