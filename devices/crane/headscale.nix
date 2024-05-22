7f7a8cbbd9625e96526e248075dc80afd8b2753d
Ankit Saini <nnkitsaini@gmail.com>
2024-05-22 23:17:06 +0530

    format

--- devices/crane/headscale.nix
+++ devices/crane/headscale.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: 
+{ config, pkgs, ... }:
 let
   domain = "headscale.nkit.dev";
   derpPort = 3478;
@@ -30,43 +26,45 @@ in {
   services = {
     headscale = {
       enable = true;
-      package = (pkgs.runCommand "headscale" { buildInputs = [ pkgs.makeWrapper ]; } ''
-      makeWrapper ${pkgs.headscale}/bin/headscale $out/bin/headscale --set HEADSCALE_EXPERIMENTAL_FEATURE_SSH 1
-  '');
+      package =
+        (pkgs.runCommand "headscale" { buildInputs = [ pkgs.makeWrapper ]; } ''
+          makeWrapper ${pkgs.headscale}/bin/headscale $out/bin/headscale --set HEADSCALE_EXPERIMENTAL_FEATURE_SSH 1
+        '');
       address = "0.0.0.0";
       port = 8888;
       settings = {
         acl_policy_path = pkgs.writeTextFile {
-    name = "headscale-acl.hujson";
-    text = builtins.toJSON {
-	    acls = [{
-	    	action="accept";
-		src=["*"];
-		dst=["*:*"];
-	    }];
-	    ssh = [
-	    {
-	    	action="accept";
-	    	src=["autogroup:member"];
-	    	dst=["autogroup:self"];
-		users=["root" "autogroup:nonroot"];
-	    }
-	    {
-	    	action="accept";
-	    	src=["*"]; # TODO: fix once headscale 0.23.0 is available on nixos (that finally has non-experimental support for ssh I guess)
-	    	dst=["*:*"];
-		users=["root" "autogroup:nonroot" "ayush" "*"];
-	    }
-	    ];
+          name = "headscale-acl.hujson";
+          text = builtins.toJSON {
+            acls = [{
+              action = "accept";
+              src = [ "*" ];
+              dst = [ "*:*" ];
+            }];
+            ssh = [
+              {
+                action = "accept";
+                src = [ "autogroup:member" ];
+                dst = [ "autogroup:self" ];
+                users = [ "root" "autogroup:nonroot" ];
+              }
+              {
+                action = "accept";
+                src = [
+                  "*"
+                ]; # TODO: fix once headscale 0.23.0 is available on nixos (that finally has non-experimental support for ssh I guess)
+                dst = [ "*:*" ];
+                users = [ "root" "autogroup:nonroot" "ayush" "*" ];
+              }
+            ];
 
-    };
-    };
+          };
+        };
         dns_config = {
           override_local_dns = true;
           nameservers = [ "1.1.1.1" ]; # TODO: and 100.100.100.100?
           base_domain = "nkit.dev";
         };
-
 
         ip_prefixes = "100.64.0.0/24";
         # prefixes = {
@@ -83,22 +81,18 @@ in {
     };
 
     caddy.virtualHosts.${domain}.extraConfig = ''
-    reverse_proxy http://localhost:${toString config.services.headscale.port}
+      reverse_proxy http://localhost:${toString config.services.headscale.port}
     '';
   };
 
-  networking.firewall.allowedUDPPorts = [derpPort];
+  networking.firewall.allowedUDPPorts = [ derpPort ];
 
   environment.etc."sysctl.d/99-tailscale.conf".text = ''
     net.ipv4.ip_forward = 1
     net.ipv6.conf.all.forwarding = 1
   '';
-  environment.variables = {
-  HEADSCALE_EXPERIMENTAL_FEATURE_SSH = "1";
-  };
-  environment.sessionVariables = {
-  HEADSCALE_EXPERIMENTAL_FEATURE_SSH = "1";
-  };
+  environment.variables = { HEADSCALE_EXPERIMENTAL_FEATURE_SSH = "1"; };
+  environment.sessionVariables = { HEADSCALE_EXPERIMENTAL_FEATURE_SSH = "1"; };
 
   environment.systemPackages = [ config.services.headscale.package ];
 }
