3b6fd357d17434bf6733ac93d23f23c669f28cb5
Ankit Saini <nnkitsaini@gmail.com>
2024-06-28 02:08:27 +0530

    [crane]systemd->grub

--- devices/crane/disco.nix
+++ devices/crane/disco.nix
@@ -1,8 +1,6 @@
-
 # NOTE: if after the install /boot gets garbled, i.e. ls /boot gives Input/Output error with weird filenames, clean and re-format /boot and run
 # sudo NIXOS_INSTALL_BOOTLOADER=1 /nix/var/nix/profiles/system/bin/switch-to-configuration boot
 # This happened once, not sure if it was one time thing or not?
-
 {
   disko.devices.disk = {
     primary = {
@@ -10,14 +8,27 @@
       # Next time use ID
       device = "/dev/sda";
       content = {
-        type = "mbr";
+        type = "gpt";
+        efiGptPartitionFirst = false;
+
         partitions = {
-          ESP = {
-            label = "boot";
-            name = "ESP";
+          TOW-BOOT-FI = {
             priority = 1;
-            size = "512M";
             type = "EF00";
+            size = "32M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = null;
+            };
+            hybrid = {
+              mbrPartitionType = "0x0c";
+              mbrBootableFlag = false;
+            };
+          };
+          ESP = {
+            type = "EF00";
+            size = "512M";
             content = {
               type = "filesystem";
               format = "vfat";
@@ -37,20 +48,20 @@
             size = "100%";
             content = {
               type = "btrfs";
-              extraArgs = [ "-f" ]; # Override existing partition
+              extraArgs = ["-f"]; # Override existing partition
               # Subvolumes must set a mountpoint in order to be mounted,
               # unless their parent is mounted
               subvolumes = {
                 "/rootfs" = {
-                  mountOptions = [ "compress=zstd:1" "noatime" ];
+                  mountOptions = ["compress=zstd:1" "noatime"];
                   mountpoint = "/";
                 };
                 "/home" = {
-                  mountOptions = [ "compress=zstd:1" "noatime" ];
+                  mountOptions = ["compress=zstd:1" "noatime"];
                   mountpoint = "/home";
                 };
                 "/nix" = {
-                  mountOptions = [ "compress=zstd:1" "noatime" ];
+                  mountOptions = ["compress=zstd:1" "noatime"];
                   mountpoint = "/nix";
                 };
               };
