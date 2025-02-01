11e4f35b4fc1b2c4f325c69f442c8dfcbdae3b19
Ankit Saini <nnkitsaini@gmail.com>
2025-02-01 14:29:00 +0530

    fix neorg

--- packages/hm/neovim/plugins/neorg.nix
+++ packages/hm/neovim/plugins/neorg.nix
@@ -1,5 +1,38 @@
 {
   programs.nixvim = {
+    extraConfigLua = ''
+      -- ----------------- neorg.dirman monkey patching start ------------------
+      -- 
+      -- https://github.com/nvim-neorg/neorg/blob/bdb29ea3e069f827d31973bc942c18793ee9fa64/lua/neorg/modules/core/dirman/module.lua#L415
+      -- 
+      local dirman = require('neorg').modules.get_module("core.dirman")
+
+      -- Monkey Patching: Adds a filter to not scan hidden directories
+      local new_get_norg_files = function(workspace_name)
+      	local res = {}
+      	local workspace = dirman.get_workspace(workspace_name)
+
+      	if not workspace then
+      		return
+      	end
+
+      	local path_filter = function(path)
+      		return path:is_hidden()
+      	end
+
+      	for path in workspace:fs_iterdir(true, 20, path_filter) do
+      		if path:is_file(true) and path:suffix() == ".norg" then
+      			table.insert(res, path)
+      		end
+      	end
+
+      	return res
+      end
+      dirman.get_norg_files = new_get_norg_files;
+
+      -- :---------------- neorg.dirman monkey patching end ------------------
+
+    '';
     keymaps = [
       {
         action = ":Neorg journal today<CR>";
@@ -65,11 +98,13 @@
           end
         '';
 
-        "core.dirman".config.workspaces = {
-          notes = "~/code/notes/norg";
+        "core.dirman".config = {
+          workspaces = {
+            notes = "~/code/notes/";
+          };
+          open_last_workspace = "default";
+          default_workspace = "notes";
         };
-
-        "core.dirman".config.default_workspace = "notes";
 
         "core.concealer".__empty = null;
         "core.summary".__empty = null;
