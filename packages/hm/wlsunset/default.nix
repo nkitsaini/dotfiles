2c55c3524c7dce5768e803ae543080aa3676c443
Ankit Saini <nnkitsaini@gmail.com>
2025-02-04 15:12:00 +0530

    fixing lat/lang

--- packages/hm/wlsunset/default.nix
+++ packages/hm/wlsunset/default.nix
@@ -1,8 +1,8 @@
 {
   services.wlsunset = {
     enable = true;
-    longitude = "20.5";
-    latitude = "78.9";
+    longitude = "78.9";
+    latitude = "20.5";
     temperature = {
       day = 6000;
       night = 1800;
