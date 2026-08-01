e08b48cec3ceba98f878059aa2ee22c8515d29c3
Ankit Saini <nnkitsaini@gmail.com>
2026-08-01 14:35:13 +0530

    use latest kernel

--- packages/os/kernel.nix
+++ packages/os/kernel.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 {
   # Let's stick with LTS for a few weeks to see if bluetooth issue persists
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   environment.systemPackages = [
     config.boot.kernelPackages.perf
   ];
